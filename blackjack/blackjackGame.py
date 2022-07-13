46faddf4aec65559ccab261d2080676e94fb362f
aria231 <aria231@students.cs.ubc.ca>
2022-07-13 00:04:02 -0700

    Clean up blackjackGame class

--- blackjack/blackjackGame.py
+++ blackjack/blackjackGame.py
@@ -3,32 +3,9 @@ from PIL import Image
 from blackjackPlayer import *
 from blackjackDeck import *
 
-# helper methods
-def dealerPrintHand():
-    print("Dealer cards:")
-    dealer.printCards()
-    print("Total:",dealer.getTotal(),end="\n")
-    return
-
-def dealerTurn():
-    inp = input('---Enter for dealer turn---')
-    while (inp != ""):
-        inp = input('---Please press enter for dealer turn---')
-    while (dealer.getTotal() < 17):
-        dealerPrintHand()
-        print("Dealer hits")
-        dealer.addCard(deck.drawCard())
-        inp = input('---Please press enter to continue---')
-        while (inp != ""):
-            inp = input('---Please press enter for to continue---')
-    dealerPrintHand()
-    if (dealer.getTotal() > 21):
-        print("Dealer busts!")
-    else:
-        print("Dealer stands")
-    return
-
 pygame.init()
+
+# ------------------------------- CONSTANTS -------------------------------
 
 WIDTH = 1500
 HEIGHT = 1000
@@ -38,11 +15,7 @@ GREEN = (46, 112, 64)
 LIGHT_GREEN = (117, 186, 117)
 RED = pygame.Color("red")
 
-# initialize screen and display
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen.fill(GREEN)
-pygame.display.set_caption("It's time to gamble!")
-pygame.display.flip()
+# ------------------------------- INITIALIZE GAME STATS -------------------------------
 
 # initialize deck and players
 deck = Deck()
@@ -50,7 +23,8 @@ deck = Deck()
 user = Player('Name')
 dealer = Player('Dealer')
 
-# Game Variables
+# ------------------------------- GAME VARIABLES -------------------------------
+
 playerTurn = user
 initMessage = "LET'S GIVE AWAY ALL YOUR MONEYYYYY ^ - ^"
 font = pygame.font.Font('freesansbold.ttf', 32)
@@ -68,10 +42,20 @@ announcerArea.center = (WIDTH // 2, HEIGHT // 10)
 # total = player.getTotal()
 # print('total: ', total)
 
+# ------------------------------- INITIALIZE PYGAME SCREEN -------------------------------
+
+# initialize screen and display
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(GREEN)
+pygame.display.set_caption("It's time to gamble!")
+pygame.display.flip()
+
 
 # =======================================================================================
 # HELPER METHODS
 # =======================================================================================
+
+# ------------------------------- OTHER HELPER METHODS -------------------------------
 
 # Change Announcer mesage
 def announceText(msg, msgColour, bckgdColour):
@@ -79,6 +63,8 @@ def announceText(msg, msgColour, bckgdColour):
     announcer = font.render(msg, True, msgColour, bckgdColour)
     screen.blit(announcer, announcerArea)
     pygame.display.update()
+
+# ------------------------------- USER METHODS -------------------------------
 
 # User takes a turn: hit or stand
 # hit -> deal a card to user's hand
@@ -106,61 +92,59 @@ def userTurn():
         else:
             print("Not a valid response. Try again...if you want to live...")
 
+# ------------------------------- DEALER METHODS -------------------------------
+
+def dealerPrintHand():
+    print("Dealer cards:")
+    dealer.printCards()
+    print("Total:",dealer.getTotal(),end="\n")
+    return
+
+def dealerTurn():
+    inp = input('---Enter for dealer turn---')
+    while (inp != ""):
+        inp = input('---Please press enter for dealer turn---')
+    while (dealer.getTotal() < 17):
+        dealerPrintHand()
+        print("Dealer hits")
+        dealer.addCard(deck.drawCard())
+        inp = input('---Please press enter to continue---')
+        while (inp != ""):
+            inp = input('---Please press enter for to continue---')
+    dealerPrintHand()
+    if (dealer.getTotal() > 21):
+        print("Dealer busts!")
+    else:
+        print("Dealer stands")
+    return
+
+
+# ------------------------------- CONSOLE TESTING -------------------------------
+
+userTurn()
 dealerTurn()
 
-running = True
-while running:
-    # Initial message to user
-    announceText(initMessage, RED, None)
-
-    # # For inlcuding images
-    # currCardImg = pygame.image.load('blackjack/Images/2C.jpg')
-    # screen.blit(currCardImg, (0, 0))
-
-    # User's turn
-    # pause = input("Press enter to continue...")
-    # announceText("IT'S YOUR TURN!! GIVE ME THAT MONEY", RED, None)
-    # userTurn()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-        # elif event.type = pygame.MOUSEBUTTONDOWN:
-        
-    # JUST TRYING STUFF OUT
-    
-    # font = pygame.font.SysFont('Corbel', 35)
-    # text = font.render('?', True, (0, 0, 0))
-    # bottom_right = ((width * 0.90), (height * 0.90))
-    # radius = 15
-    # pygame.draw.circle(screen, LIGHT_GREEN, bottom_right, radius )
-    # screen.blit(text, bottom_right)
-    # pygame.display.update()
-
-    # mouse = pygame.mouse.get_pos()
-    # if bottom_right[0] + 2 * radius > mouse[0] > bottom_right[0] and bottom_right[1] + 2 * radius > mouse[1] > bottom_right[1]:
-    #     pygame.draw.circle(screen, (0, 0, 0), bottom_right, radius)
-    # else:
-    #     pygame.draw.circle(screen, LIGHT_GREEN, bottom_right, radius)
-    # pygame.display.update()
-    
-    # pygame.draw.circle(screen, LIGHT_GREEN, bottom_right, radius)
-    pygame.display.update()
-            
-
-
+#  ------------------------------- PYGAME -------------------------------
 
 # running = True
 # while running:
+#     # Initial message to user
+#     announceText(initMessage, RED, None)
+
+#     # # For inlcuding images
+#     # currCardImg = pygame.image.load('blackjack/Images/2C.jpg')
+#     # screen.blit(currCardImg, (0, 0))
+
+#     # User's turn
+#     # pause = input("Press enter to continue...")
+#     # announceText("IT'S YOUR TURN!! GIVE ME THAT MONEY", RED, None)
+#     # userTurn()
+
 #     for event in pygame.event.get():
 #         if event.type == pygame.QUIT:
 #             running = False
-#     screen.fill((255, 255, 255));
-
-#     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-#     pygame.display.flip()
-# pygame.quit()
+#             pygame.quit()
+#         # elif event.type = pygame.MOUSEBUTTONDOWN:
+#     pygame.display.update()
 
 
